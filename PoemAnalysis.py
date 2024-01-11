a198697639fa174cab12fc4b93e456f9ec684444
James Faulkner <jamesffaulkner@gmail.com>
2024-01-11 18:25:00 -0500

    Improved comments and standardization in PoemAnalysis.py

--- PoemAnalysis.py
+++ PoemAnalysis.py
@@ -10,16 +10,16 @@ def read_file(file_name):
         return "File not found."
 
 #Poem analyzer that starts a chat completions; system and user instructions are prefabricated
-def analyze_poem_chat(sample, poem, model="gpt-3.5-turbo"):
+def analyze_poem(sample, poem, model="gpt-3.5-turbo"):
     messages = [{"role": "system", "content": "Assistant provides detailed analyses of poems. Assistant is familiar with these poetic terms: simile, metaphor, metonymy, imagery, synecdoche, meter, diction, end rhyme, internal rhyme, and slant rhyme."},
                 {"role": "user", "content": f"Base analysis and output on these samples. The sample poems are delimited by the xml tags <poem> and </poem>. The sample analyses follow their respective sample poems and are delimited by xml tags <analysis> and </analysis>. /n/nSample: {sample}./n/nPlease analyze this poem delimited by <poem> and </poem> XML tags:\n\n{poem}"}]
     
-    MyAI = OpenAI(
-        #Initialize api_key = "INSERT YOUR API KEY AS A STRING" 
-        #or follow other OpenAI API methods detailed in online docs
-        #(e.g., editing your bash_profile) 
+    client = OpenAI(
+        # Initialize api_key = "INSERT_YOUR_API_KEY_AS_STRING" 
+        # or use .env file with Python-dotenv module
+        # or edit bash_profile—see OpenAI API quickstart instructions
     )
-    response = MyAI.chat.completions.create(
+    response = client.chat.completions.create(
         model=model,
         messages=messages,
         temperature=0.2
@@ -32,12 +32,13 @@ def analyze_poem_chat(sample, poem, model="gpt-3.5-turbo"):
 
 # Get samples
 
-file_path = "PoemAnalysisSamples.txt"  # Replace with the path to your file
-sample_poems = read_file(file_path)
+sample_path = "PoemAnalysisSamples.txt" 
+sample_poems = read_file(sample_path)
 
-# Analyzing a new poem (replace with the poem you want to analyze)
-second_file_path = "DesertPlaces.txt"
-new_poem = read_file(second_file_path)
-analysis = analyze_poem_chat(sample_poems, new_poem)
+# Get a new poem to analyze
+new_poem_path = "DesertPlaces.txt" # Replace with path to your new poem file
+new_poem = read_file(new_poem_path)
 
+#Get and print ChatGPT response
+analysis = analyze_poem(sample_poems, new_poem)
 print(analysis)
